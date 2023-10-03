382d5f5c6f9c8833a2441874ce23d2410e56362f
ntminh181294 <ntminh181294@gmail.com>
2023-10-03 18:40:03 +0700

    Add project files.

---
+++ Lab2-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap so giay: ");
+        int soGiay = int.Parse(Console.ReadLine());
+
+        int gio = soGiay / 3600;
+        int phut = (soGiay % 3600) / 60;
+        int giay = soGiay % 60;
+
+        string gioStr = gio.ToString().PadLeft(2, '0');
+        string phutStr = phut.ToString().PadLeft(2, '0');
+        string giayStr = giay.ToString().PadLeft(2, '0');
+
+        Console.WriteLine($"Dinh dang gio:phut:giay la {gioStr}:{phutStr}:{giayStr}");
+    }
+}
